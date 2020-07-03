3ca945a08e788d66b36ad5c2144ef5cc26ca1f99
alex rasmussen <arasmussen64@gmail.com>
2020-07-02 18:51:25 -0600

    created boozes_controller

--- db/migrate/20200701023617_add_trackable_to_users.rb
+++ db/migrate/20200701023617_add_trackable_to_users.rb
@@ -1,6 +1,6 @@
 class AddTrackableToUsers < ActiveRecord::Migration[6.0]
   def change
-    def change
+   
       ## Trackable
       add_column :users, :sign_in_count, :integer, :default => 0
       add_column :users, :current_sign_in_at, :datetime
@@ -8,5 +8,5 @@ class AddTrackableToUsers < ActiveRecord::Migration[6.0]
       add_column :users, :current_sign_in_ip, :string
       add_column :users, :last_sign_in_ip, :string
   end
-  end
+
 end
