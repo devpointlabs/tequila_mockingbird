75e7a1d6fc6b98632994df5aaa698c7ec2855473
Karina Stevens <keva86@hotmail.com>
2020-07-06 20:51:32 -0600

    update drink form with booze id

--- client/src/components/Drinks/DrinkForm.js
+++ client/src/components/Drinks/DrinkForm.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import Drink from './Drink';
+import axios from "axios";
 
 class DrinkForm extends React.Component {
-  state = { name: "", history: "", ingredients: "", prep_serv: ""};
+  state = { name: "", 
+  history: "", 
+  ingredients: "", 
+  prep_serv: "", 
+  boozeChoice: null,
+  boozes: [],
+};
 
   componentDidMount() {
     if (this.props.drink) {
       const {  name, history, ingredients, prep_serv } = this.props.drink;
       this.setState({ name: name, history: history, ingredients: ingredients, prep_serv: prep_serv});
     }
+    // axios call to booze controller index method
+    // to grab all boozes, set them to booze array 
+    axios .get('/api/boozes')
+    .then((res) => {
+      this.setState({ boozes: res.data });
+    })
+    .catch(console.log("Party Foul"));
+
   }
 
   handleChange = (e) => {
@@ -28,9 +43,19 @@ class DrinkForm extends React.Component {
       this.props.toggleForm();
     }
   };
+// function that is going to list out all booze
+  addBoozeToDrink = () => {
+    return this.state.boozes.map( (booze) => (
+      <option value={booze.id}>
+        {booze.name}
+      </option>
+
+    ))
+  }
+
 
   render() {
-    const { name, history, ingredients, prep_serv } = this.state;
+    const { name, history, ingredients, prep_serv, boozeChoice } = this.state;
     return (
       <form onSubmit={this.handleSubmit}>
         <input
@@ -50,7 +75,18 @@ class DrinkForm extends React.Component {
           name="ingredients"
           value={ingredients}
           onChange={this.handleChange}
-        />
+        />  
+        {/* lists out all of the booze names */}
+        <select 
+          placeholder="Alcohol"
+          // be a radio list where you can selected more then one booze
+          name="boozeChoice" 
+          value={boozeChoice}
+          onChange={this.handleChange}
+          >
+          {this.addBoozeToDrink()}
+        </select>
+
         <input
           placeholder="Preparation"
           name="prep_serv"
