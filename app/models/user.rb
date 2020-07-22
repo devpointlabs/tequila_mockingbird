169eafb2d18e621ed89de4e03be05006d68b8fa7
Harlan Evans <35747338+harlanevans@users.noreply.github.com>
2020-07-21 20:32:32 -0600

    Update user.rb
    removed audit from User

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@
 
 class User < ActiveRecord::Base
   has_many :reviews, through: :boozedrinks
-  has_associated_audits
-
+  
   extend Devise::Models
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
